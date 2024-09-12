f4fc0716310b8d393f1bf8860c54d256cede4b6c
WakefieldBatty <wakefield.batty@ucas-edu.net>
2024-09-12 04:23:58 +0000

    added way too many expressions

---
+++ Expressoins_Practice.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+    printf("%d", (int) 7- (int) 24/ (int) 8* (int) 4+ (int) 6);
+    printf("%d", (int) 18/ (int) 3- (int) 7+ (int) 2* (int) 5);
+    printf("%d", (int) 6* (int) 4/ (int) 12+ (int) 72/ (int) 8- (int) 9);
+    printf("%d", ((int) 17- (int) 6/ (int) 2)+ (int) 4* (int) 3);
+    printf("%d", - (int) 2*((int) 1* (int) 4- (int) 2/ (int) 2)+( (int) 6+ (int) 2- (int) 3));
+    printf("%d", - (int) 1*(( (int) 3- (int) 4* (int) 7)/ (int) 5)- (int) 2* (int) 24/ (int) 6 );
+    printf("%d", ((int)3*(int) pow(5,2) / (int) 15)-((int) 5- (int) pow(2,2)) );
+    printf("%d", ((int) pow(1,4)* (int) pow(2,2) + (int) pow(3,3))- (int) pow(2,5)/ (int) 4 );
+    printf("%d", (int) pow(((int) 22/ (int) 2- (int) 2*(int) 5),2)+(int) pow(((int) 4- (int) 6/ (int) 6),2));
+    return 0;
+}
