f99733071ae80c88726fdae4e361dd22b75840bb
WakefieldBatty <wakefield.batty@ucas-edu.net>
2024-09-13 19:17:18 +0000

    calculator

--- expressions/Budget_Calculator.c
+++ expressions/Budget_Calculator.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
 
 int main(void){
-    char income[9];
-    char utilities[9];
-    char groceries[9];
-    char bus[9];
+    float income, rent, utilities, grocieries, transportation, savings, expenses, spend;
+    float prent, putilities, pgrocieries, ptransportation, psavings, pexpenses;
     printf("This is a budget calculator.\n How much do you make a month?\n");
-    scanf("%s", income);
+    scanf("%f", &income);
     printf("How much do your utilities cost?\n");
-    scanf("%s", utilities);
-    printf("you make %s\n", income);
-    float spend[] = (float) income - (float) utilities;
-    printf("%s", spend);
+    scanf("%f", &utilities);
+    printf("How much do your grocieries cost?\n");
+    scanf("%f", &grocieries);
+    printf("How much do your transportation cost?\n");
+    scanf("%f", &transportation);
+    savings = income * .2; 
+    expenses = rent + utilities + grocieries + transportation;
+    spend = income - expenses - savings;
+    printf("you make $ %.2f\n", income);
+    printf ("your expenses are $ %.2f\n", expenses);
+    printf("your savings are $ %.2f\n", savings);
+    printf("your spending money is $ %.2f\n", spend);
+    prent = rent / income * 100;
+    putilities = utilities / income * 100;
+    pgrocieries = grocieries / income * 100;
+    ptransportation = transportation / income * 100;
+     psavings = savings / income * 100;
+     pexpenses = expenses / income * 100;
+     printf(" your rent is %d%% of your income.\n", (int) prent);
+      printf(" your rent is %d%% of your income.\n", (int) putilities);
+       printf(" your rent is %d%% of your income.\n", (int) pgrocieries);
+        printf(" your rent is %d%% of your income.\n", (int) ptransportation);
+         printf(" your rent is %d%% of your income.\n", (int) psavings);
+          printf(" your rent is %d%% of your income.\n", (int) pexpenses);
     return 0;
 }
