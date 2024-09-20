92ea37ec9d29f0f308dd8a95b1bab8bb2ee9224f
WakefieldBatty <wakefield.batty@ucas-edu.net>
2024-09-20 03:00:42 +0000

    made silly sentence maker in python

--- Strings/Silly_Sentance/Silly_Sentance.py
+++ Strings/Silly_Sentance/Silly_Sentance.py
@@ -1,2 +1,4 @@
-a = input("Give me a noun: ");
-b = input("Give me a verb: ");
+animal = input("Name an animal: ");
+place = input("Name a place: ");
+verb = input("Name a verb: ");
+print("The", animal, "went to the", place, "and did", verb, "with their frind the angry turtle.")
