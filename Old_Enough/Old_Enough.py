af86a64a69bd46d8a7973f48e34d54b116ef5186
WakefieldBatty <wakefield.batty@ucas-edu.net>
2024-09-30 19:36:55 +0000

    made old enough code

---
+++ Old_Enough/Old_Enough.py
@@ -0,0 +1,11 @@
+age = 21
+if age >= 18:
+    print("Your are old enough to vote!") 
+if age >= 16:
+    print("Your are old enough to drive!") 
+if age >= 15:
+    print("Your are old enough to get your learners permit!") 
+if age >= 5:
+    print("Your are old enough to go to school!") 
+else:
+    print("You're not old enough to go to school. Also, why are you even using this?!?!")
