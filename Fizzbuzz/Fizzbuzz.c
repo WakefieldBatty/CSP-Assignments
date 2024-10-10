adfb1683a5322a4ef1451ee8411b44827b6847e2
WakefieldBatty <wakefield.batty@ucas-edu.net>
2024-10-10 19:41:58 +0000

    more gooderer stuff

--- Fizzbuzz/Fizzbuzz.c
+++ Fizzbuzz/Fizzbuzz.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
+#include <stdio.h>
 int i;
 int usr;
+char one[10000];
+char two[10000];
+char three[10000];
 int main(void){
     printf("Give me a number: ");
     scanf("%d", &usr);
+    printf("Give me a short word: ");
+    scanf("%s", &one);
+    printf("Give me another short word: ");
+    scanf("%s", &two);
+    strcat(three, one);
+    strcat(three, two);
     //loop that counts to 50
     for(i=1;i<=usr;i++){
         //replace #'s divisible by 3 and 5 with "FizzBuzz"
         if(i%3==0 && i%5==0){
-            printf("FizzBuzz\n");
+            printf("%s\n", three);
+
         }else if (i%3==0){
 
             //replace #'s divisible by 3 with "Fizz"
-            printf("Fizz\n");
+            printf("%s\n", one);
             //replace #'s divisible by 5 with "Buzz"
         }else if(i%5==0){
-                printf("Buzz\n");
+                printf("%s\n", two);
     
         //print the number
         }else {
